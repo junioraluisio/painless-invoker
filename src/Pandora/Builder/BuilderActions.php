71603a49a5e19c91c9aecb6470db7a9c76a9b42f
Aluisio Martins Junior <junior@mjpsolucoes.com.br>
2017-05-06 12:28:37 -0300

    Up namespace

---
+++ src/Pandora/Builder/BuilderActions.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Junior
+ * Date: 30/04/2017
+ * Time: 18:35
+ */
+
+namespace Pandora\Builder;
+
+
+class BuilderActions
+{
+    /**
+     * Escreve o arquivo
+     *
+     * @return string
+     */
+    public function write(): string
+    {
+        $actions = ['Insert','Update','Enable','Disable'];
+    
+        foreach ($actions as $action) {
+        
+        }
+        
+        return true;
+    }
+    
+}
